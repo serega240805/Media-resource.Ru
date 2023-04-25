bb27d7765f5a4ebe7734e074b71857f0a332b7d7
serega240805 <ssuchalkin@yandex.ru>
2023-04-25 18:11:02 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 'use strict';
 
-const tabs = document.querySelector(".services__tabs");
-const tabButton = document.querySelectorAll(".tab-button");
-const contents = document.querySelectorAll(".content");
+const goTopBtn = document.querySelector(".go-top");
 
-tabs.onclick = e => {
-    const id = e.target.dataset.id;
-    if (id) {
-        tabButton.forEach(btn => {
-            btn.classList.remove("active");
-        });
-        e.target.classList.add("active");
 
-        contents.forEach(content => {
-            content.classList.remove("active");
-        });
-        const element = document.getElementById(id);
-        element.classList.add("active");
+window.addEventListener("scroll", trackScroll);
+
+goTopBtn.addEventListener("click", goTop);
+
+function trackScroll() {
+
+    const scrolled = window.pageYOffset;
+
+    const coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+
+        goTopBtn.classList.add("go-top--show");
+    } else {
+
+        goTopBtn.classList.remove("go-top--show");
+    }
+}
+
+function goTop() {
+
+    if (window.pageYOffset > 0) {
+        // скроллим наверх
+        window.scrollBy(0, -35);
+        setTimeout(goTop, 0);
     }
 }
