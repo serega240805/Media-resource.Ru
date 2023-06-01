4c32a1a0c95b37f2751fac22e66ff1756fb760c2
serega240805 <ssuchalkin@yandex.ru>
2023-06-01 20:10:16 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,40 +1,61 @@
 'use strict';
 
-const tabs = document.querySelector(".services__tabs");
-const tabButton = document.querySelectorAll(".tab-button");
-const contents = document.querySelectorAll(".content");
+import anime from './node_modules/animejs/lib/anime.es.js';
 
-tabs.onclick = e => {
-    const id = e.target.dataset.id;
-    if (id) {
-        tabButton.forEach(btn => {
-            btn.classList.remove("active");
-        });
-        e.target.classList.add("active");
+const number = [12, 1258, 300, 35],
+    numberElement = document.querySelectorAll('.header__num');
 
-        contents.forEach(content => {
-            content.classList.remove("active");
-        });
-        const element = document.getElementById(id);
-        element.classList.add("active");
-    }
-}
-
-// На верх
-
-const btnUp = document.querySelector('.btn-up');
-
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 200) {
-        btnUp.classList.add('show');
-    } else {
-        btnUp.classList.remove('show');
-    }
+numberElement.forEach((element, index) => {
+    anime({
+        targets: element,
+        innerHTML: [0, number[index]],
+        duration: 5000,
+        round: 1,
+        easing: 'easeOutExpo'
+    });
 });
 
-btnUp.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
+window.addEventListener('DOMContentLoaded', () => {
+
+    //  tabs
+
+    const tabs = document.querySelector(".services__tabs"),
+    tabButton = document.querySelectorAll(".tab-button"),
+    contents = document.querySelectorAll(".content");
+
+    tabs.onclick = e => {
+        const id = e.target.dataset.id;
+        if (id) {
+            tabButton.forEach(btn => {
+                btn.classList.remove("active");
+            });
+            e.target.classList.add("active");
+
+            contents.forEach(content => {
+                content.classList.remove("active");
+            });
+            const element = document.getElementById(id);
+            element.classList.add("active");
+        }
+    }
+
+    // button-up
+
+    const btnUp = document.querySelector('.btn-up');
+
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 200) {
+            btnUp.classList.add('show');
+        } else {
+            btnUp.classList.remove('show');
+        }
     });
+
+    btnUp.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
 });
